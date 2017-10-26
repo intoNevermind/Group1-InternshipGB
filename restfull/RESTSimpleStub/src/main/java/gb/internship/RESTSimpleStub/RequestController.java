2d543699a471b420999a7c22f2f9b2c6184534a1
Aleksandr Vvedensky <slin2006@yandex.ru>
2017-10-26 15:48:36 +0300

    Добавлен метод для логирования.

--- restfull/RESTSimpleStub/src/main/java/gb/internship/RESTSimpleStub/RequestController.java
+++ restfull/RESTSimpleStub/src/main/java/gb/internship/RESTSimpleStub/RequestController.java
@@ -1,5 +1,7 @@
 package gb.internship.RESTSimpleStub;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class RequestController {
 
+    private Log LOG = LogFactory.getLog(RequestController.class);
     public static final String DEFAULT_RESPONSE = "You see default string." +
             "<br>Use: http://localhost:8080/echo?echoString=myString" +
             "<br>to see your string.";
@@ -26,5 +29,17 @@ public class RequestController {
     public String echo(
             @RequestParam(value = "echoString", defaultValue = DEFAULT_RESPONSE) String echoString) {
         return echoString;
+    }
+
+
+    /**
+     * Выводит сообщения в лог, если if(true)
+     *
+     * @param msg сообщения для лога.
+     */
+    private void printToLog(String msg) {
+        if (true) {
+            LOG.info(msg);
+        }
     }
 }
