7fd89323209ddf5b8d291417e2359887fcd5f75b
aazarodov <aazarodov@gmail.com>
2017-10-27 10:49:29 +0300

    IF NOT EXISTS

--- db_crawler/db/script db.sql
+++ db_crawler/db/script db.sql
@@ -1,13 +1,13 @@
-CREATE SCHEMA `persons_statistics`;
+CREATE SCHEMA IF NOT EXISTS `persons_statistics`;
 
-CREATE TABLE `persons_statistics`.`persons` (
+CREATE TABLE IF NOT EXISTS `persons_statistics`.`persons` (
   `ID` INT NOT NULL AUTO_INCREMENT,
   `Name` NVARCHAR(2048) NOT NULL,
   `Active` BOOLEAN DEFAULT TRUE NOT NULL,
   PRIMARY KEY (`ID`))
 COMMENT = 'Таблица базы данных, отвечающая за хранение имен личностей. Каждой личности соответствует от 0 до бесконечности ключевых слов.';
 
-CREATE TABLE `persons_statistics`.`keywords` (
+CREATE TABLE IF NOT EXISTS `persons_statistics`.`keywords` (
   `ID` INT NOT NULL AUTO_INCREMENT,
   `Name` NVARCHAR(2048) NOT NULL,
   `PersonID` INT NOT NULL COMMENT 'Идентификатор личности, которой соответствует данное ключевое слово. Является внешним ключом к таблице Persons.',
@@ -20,7 +20,7 @@ CREATE TABLE `persons_statistics`.`keywords` (
     ON UPDATE NO ACTION)
 COMMENT = 'Tаблица базы данных, отвечающая за хранение ключевых слов, соответствующих каждой конкретной личности.';
 
-CREATE TABLE `persons_statistics`.`sites` (
+CREATE TABLE IF NOT EXISTS  `persons_statistics`.`sites` (
   `ID` INT NOT NULL AUTO_INCREMENT,
   `Name` NVARCHAR(2048) NOT NULL,
   `URL` NVARCHAR(2048) NOT NULL,
@@ -28,7 +28,7 @@ CREATE TABLE `persons_statistics`.`sites` (
   PRIMARY KEY (`ID`))
 COMMENT = 'Tаблица базы данных, содержит названия сайтов для анализа на упоминания.';
 
-CREATE TABLE `persons_statistics`.`pages` (
+CREATE TABLE IF NOT EXISTS `persons_statistics`.`pages` (
   `ID` INT NOT NULL AUTO_INCREMENT,
   `URL` NVARCHAR(2048) NOT NULL,
   `SiteID` INT NOT NULL COMMENT 'Идентификатор сайта (ресурса), который предоставлен  администратором для анализа. Является внешним ключом к таблице Sites.',
@@ -43,7 +43,7 @@ CREATE TABLE `persons_statistics`.`pages` (
     ON UPDATE NO ACTION)
 COMMENT = 'Таблица базы данных, содержит страницы сайта, которые были найдены при анализе сайтов из таблицы Sites.';
 
-CREATE TABLE `persons_statistics`.`personpagerank` (
+CREATE TABLE IF NOT EXISTS `persons_statistics`.`personpagerank` (
   `PersonID` INT NOT NULL COMMENT 'Идентификатор личности, которой соответствует данное ключевое слово. Является внешним ключом к таблице Persons.',
   `PageID` INT NOT NULL COMMENT 'Идентификатор страницы сайта, на которой найдены упоминания о персонах. Является внешним ключом к таблице Pages.',
   `Rank` INT NOT NULL COMMENT 'Количество упоминаний личности на странице.',
