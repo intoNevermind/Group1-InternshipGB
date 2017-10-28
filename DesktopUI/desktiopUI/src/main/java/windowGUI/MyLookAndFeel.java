cbd37c9b6af075b826d21244876c4f0a187111f1
Niove1 <legepard@mail.ru>
2017-10-28 21:04:38 +0300

    Создан класс для отображения фейковых данных.Добавленны классы для стилизации внешнего вида приложения, добавлен листнер на кнопку общей статистики.

---
+++ DesktopUI/desktiopUI/src/main/java/windowGUI/MyLookAndFeel.java
@@ -0,0 +1,25 @@
+package windowGUI;
+
+import javax.swing.plaf.basic.BasicLookAndFeel;
+
+public class MyLookAndFeel extends BasicLookAndFeel {
+    public String getName() {
+        return "Cross-platform Java Look and Feel";
+    }
+
+    public String getID() {
+        return "MyLookAndFeel";
+    }
+
+    public String getDescription() {
+        return getName();
+    }
+
+    public boolean isNativeLookAndFeel() {
+        return false;
+    }
+
+    public boolean isSupportedLookAndFeel() {
+        return true;
+    }
+}
--- DesktopUI/desktiopUI/src/main/java/windowGUI/launchApplication.java
+++ DesktopUI/desktiopUI/src/main/java/windowGUI/launchApplication.java
@@ -1,7 +1,24 @@
 package windowGUI;
 
+import javax.swing.*;
+
 public class launchApplication {
     public static void main(String[] args) {
+        setStyleLookAndFeel();
         new SelectionOfStatistics();
+    }
+
+    public static void setStyleLookAndFeel(){
+        try {
+            UIManager.setLookAndFeel(MyLookAndFeel.class.getCanonicalName());
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (UnsupportedLookAndFeelException e) {
+            e.printStackTrace();
+        }
     }
 }
