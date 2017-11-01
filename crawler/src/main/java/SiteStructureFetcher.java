a54c3446402e5a19f48d33d078c9a1fac56208b9
TheFernflower <tanu.foto@gmail.com>
2017-11-01 22:55:09 +0100

    А теперь точно добавила :)

--- crawler/src/main/java/SiteStructureFetcher.java
+++ crawler/src/main/java/SiteStructureFetcher.java
@@ -1,7 +1,40 @@
-package PACKAGE_NAME;
-
 /**
  * Created by ferney on 01.11.17.
  */
 public class SiteStructureFetcher {
+
+    public static void updateSiteStructure(String url){
+        String[] sitemapUrls = fetchSitemaps(url);
+
+        if (sitemapUrls.length==0){
+            crawlPage(url);
+        }
+        else crawlSitemaps(sitemapUrls);
+
+    }
+
+    private static String[] fetchSitemaps(String url){
+        //проверяет есть ли robots.txt и если он есть, то есть ли ссылки на sitemap
+        //если нет sitemap то возвращает пустой массив, иначе записывает в массив все найденные ссылки
+        //нет robots.txt возвращаем так же пустой массив
+    }
+
+    //2 разных метода, потому что у некоорых сайтов есть sitemap и все ссылки можно получить из него, а у некоторых нет :(
+    //и все ссылки придется получать разбирая HTML страниц
+
+    private static void crawlSitemaps(String[] urls){
+        //находим все ссылки на странице
+        //запускаем для каждой crawlSitemap
+        //записываем в базу страницу с ссылкой на текущий sitemap
+        //вопрос oграничивать ли глубину и проверять ли повторы
+    }
+
+    private static void crawlPage(String url){
+        //находим все ссылки на странице
+        //запускаем для каждой crawlPage
+        //записываем в базу адрес страницы
+        //вопрос oграничивать ли глубину и проверять ли повторы
+    }
+
+    //TODO классы обертки для базы - добавление страницы, проверка на наличие страницы
 }
--- crawler/src/test/java/DownloaderTest.java
+++ crawler/src/test/java/DownloaderTest.java
@@ -1,8 +1,23 @@
-import static org.junit.Assert.*;
+import org.junit.Assert;
+import org.junit.Test;
 
 /**
  * Created by ferney on 28.10.17.
  */
 public class DownloaderTest {
+
+
+    Downloader downloader = new Downloader();
+
+    @Test (expected = DownloadException.class)
+    public void downloadExc() throws Exception {
+        downloader.download("http://ya.ru/java/java");
+    }
+
+    @Test
+    public void download() throws Exception {
+        String keyWord = "Яндекс1";
+        Assert.assertTrue(downloader.download("http://ya.ru").matches(".*" + keyWord + ".*"));
+    }
 
 }
