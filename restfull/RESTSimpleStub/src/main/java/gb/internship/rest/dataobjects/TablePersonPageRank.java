9a8b504421c3600556571524e47569272d0195ab
agcheb <agchebykin@gmail.com>
2017-11-01 09:40:13 +0300

    Таблица страниц(TablePages) для связи таблицы уровня популярности и таблицы сайтов

--- restfull/RESTSimpleStub/src/main/java/gb/internship/rest/dataobjects/TablePersonPageRank.java
+++ restfull/RESTSimpleStub/src/main/java/gb/internship/rest/dataobjects/TablePersonPageRank.java
@@ -4,4 +4,37 @@ package gb.internship.rest.dataobjects;
  * Created by agcheb on 31.10.17.
  */
 public class TablePersonPageRank {
+    private Integer personID;
+    private Integer pageID;
+    private Integer rank;
+
+    public TablePersonPageRank(Integer personID, Integer pageID, Integer rank) {
+        this.personID = personID;
+        this.pageID = pageID;
+        this.rank = rank;
+    }
+
+    public Integer getPersonID() {
+        return personID;
+    }
+
+    public void setPersonID(Integer personID) {
+        this.personID = personID;
+    }
+
+    public Integer getPageID() {
+        return pageID;
+    }
+
+    public void setPageID(Integer pageID) {
+        this.pageID = pageID;
+    }
+
+    public Integer getRank() {
+        return rank;
+    }
+
+    public void setRank(Integer rank) {
+        this.rank = rank;
+    }
 }
