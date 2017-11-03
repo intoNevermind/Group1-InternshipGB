2dcf8c12d8cf3adec663ef80786e776e2ac01442
agcheb <agchebykin@gmail.com>
2017-11-03 17:56:20 +0300

    добавлен соединение с БД в классUsersUiDbOperation

--- restfull/RESTSimpleStub/src/main/java/gb/internship/rest/db/operations/UsersUiSitesDbOperation.java
+++ restfull/RESTSimpleStub/src/main/java/gb/internship/rest/db/operations/UsersUiSitesDbOperation.java
@@ -1,5 +1,11 @@
 package gb.internship.rest.db.operations;
 
+import gb.internship.rest.db.DbWrapper;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import java.sql.Connection;
+
 /**
  * @author баранов
  * Класс для работы с базой с User UI
@@ -7,4 +13,12 @@ package gb.internship.rest.db.operations;
 
 
 public class UsersUiSitesDbOperation {
+    private Log LOG = LogFactory.getLog(UsersUiSitesDbOperation.class);
+
+    private Connection connection;
+
+    public UsersUiSitesDbOperation() {
+        this.connection = DbWrapper.getInstance().getConnection();
+    }
+
 }
