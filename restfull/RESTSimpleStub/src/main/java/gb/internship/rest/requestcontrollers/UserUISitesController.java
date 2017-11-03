eefee859d0d91bcf66b76f39f5d0f755a037a593
agcheb <agchebykin@gmail.com>
2017-11-03 18:00:05 +0300

    userrestcontroller продолжение

--- restfull/RESTSimpleStub/src/main/java/gb/internship/rest/requestcontrollers/UserUISitesController.java
+++ restfull/RESTSimpleStub/src/main/java/gb/internship/rest/requestcontrollers/UserUISitesController.java
@@ -1,7 +1,24 @@
 package gb.internship.rest.requestcontrollers;
 
+import gb.internship.rest.db.operations.UsersUiSitesDbOperation;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.SQLException;
+
 /**
  * Created by agcheb on 03.11.17.
  */
+@RestController
 public class UserUISitesController {
+
+    private Log LOG = LogFactory.getLog(UserUISitesController.class);
+
+    private UsersUiSitesDbOperation userUiSitesDbOperations;
+
+    public UserUISitesController() throws SQLException {
+        userUiSitesDbOperations = new UsersUiSitesDbOperation();
+    }
+
 }
