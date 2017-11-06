ffb3d336676be86d84e8dbf6cee49c44ecc992a5
alexprigor <prigor5@hotmail.com>
2017-11-06 20:03:17 +0300

    Добавлены параметры метода

--- crawler/src/main/java/PersonRankUpdater.java
+++ crawler/src/main/java/PersonRankUpdater.java
@@ -1,9 +1,11 @@
+import java.util.ArrayList;
+
 /**
  * Created by ferney on 01.11.17.
  */
 public class PersonRankUpdater {
 
-    public static void updatePersonRanks(){
+    public static void updatePersonRanks(ArrayList<String> linksFromDbPages, ArrayList<String> keyWords){
         //проходим по массиву ссылок
         //к каждой странице применяем downloader
         //ищем и считаем ключевые слова
