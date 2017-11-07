ad52434e0b0986f4f3887fd6546b4901cfe40577
agcheb <agchebykin@gmail.com>
2017-11-07 22:16:01 +0300

    класс обертка для статистики

--- restfull/RESTSimpleStub/src/main/java/gb/internship/rest/dataobjects/PersonGeneralStatistic.java
+++ restfull/RESTSimpleStub/src/main/java/gb/internship/rest/dataobjects/PersonGeneralStatistic.java
@@ -4,4 +4,38 @@ package gb.internship.rest.dataobjects;
  * Created by agcheb on 07.11.17.
  */
 public class PersonGeneralStatistic {
+    private Integer id;
+    private String name;
+    private Integer generalRank;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getGeneralRank() {
+        return generalRank;
+    }
+
+    public void setGeneralRank(Integer generalRank) {
+        this.generalRank = generalRank;
+    }
+
+    public PersonGeneralStatistic(Integer id, String name, Integer generalRank) {
+
+        this.id = id;
+        this.name = name;
+        this.generalRank = generalRank;
+    }
 }
