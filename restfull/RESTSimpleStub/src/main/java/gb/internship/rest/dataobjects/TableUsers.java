eb33da3c3261272a76b76aed8f2f33154b2a5ab7
Aleksandr Vvedensky <slin2006@yandex.ru>
2017-11-07 16:44:50 +0300

    В TableUsers добавлено недостающее поле.

--- restfull/RESTSimpleStub/src/main/java/gb/internship/rest/dataobjects/TableUsers.java
+++ restfull/RESTSimpleStub/src/main/java/gb/internship/rest/dataobjects/TableUsers.java
@@ -1,37 +1,63 @@
 package gb.internship.rest.dataobjects;
 
-/**@author баранов
+/**
  * Класс для работы с таблицей Users
+ *
+ * @author баранов
+ * @author Aleksandr Vvedensky
  */
-
-
-
 public class TableUsers {
     private Integer id;
     private String login;
-    private  boolean admin;
-    private  String password;
+    private boolean admin;
+    private String password;
+    private boolean active;
 
-    public Integer getId() {return id;}
+    public Integer getId() {
+        return id;
+    }
 
-    public void setId(Integer id) {this.id = id;}
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
-    public String getLogin() {return login;}
+    public String getLogin() {
+        return login;
+    }
 
-    public void setLogin(String login) {this.login = login;}
+    public void setLogin(String login) {
+        this.login = login;
+    }
 
-    public boolean isAdmin() {return admin;}
+    public boolean isAdmin() {
+        return admin;
+    }
 
-    public void setAdmin(boolean admin) {this.admin = admin;}
+    public void setAdmin(boolean admin) {
+        this.admin = admin;
+    }
 
-    public String getPassword() {return password;}
+    public String getPassword() {
+        return password;
+    }
 
-    public void setPassword(String password) {this.password = password;    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
-    public TableUsers(Integer id, String login, boolean admin, String password) {
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public TableUsers(Integer id, String login, boolean admin, String password, boolean active) {
         this.id = id;
         this.login = login;
         this.admin = admin;
         this.password = password;
+        this.active = active;
     }
 }
