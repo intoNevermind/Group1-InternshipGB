ee722a7e9c50a387a94cb8c65df1bcd971b0e285
agcheb <agchebykin@gmail.com>
2017-11-13 19:55:14 +0300

    Контроллер и класс с запросами к Таблице личностей. Добавлен запрос на список ID

---
+++ restfull/RESTSimpleStub/src/main/java/gb/internship/rest/db/operations/TablePersonOperations.java
@@ -0,0 +1,7 @@
+package gb.internship.rest.db.operations;
+
+/**
+ * Created by agcheb on 13.11.17.
+ */
+public class TablePersonOperations {
+}
---
+++ restfull/RESTSimpleStub/src/main/java/gb/internship/rest/requestcontrollers/user/TablePersonController.java
@@ -0,0 +1,7 @@
+package gb.internship.rest.requestcontrollers.user;
+
+/**
+ * Created by agcheb on 13.11.17.
+ */
+public class TablePersonController {
+}
