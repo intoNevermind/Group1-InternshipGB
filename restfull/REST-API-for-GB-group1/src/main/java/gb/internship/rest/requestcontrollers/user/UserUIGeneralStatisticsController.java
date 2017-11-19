a88728867c7b24881cc92062fbe8060237a3324e
agcheb <agchebykin@gmail.com>
2017-11-19 21:00:49 +0300

    исправлена ошибка в запросе, добавлен ответ с сервера при неудачном исполнении запроса

--- restfull/REST-API-for-GB-group1/src/main/java/gb/internship/rest/requestcontrollers/user/UserUIGeneralStatisticsController.java
+++ restfull/REST-API-for-GB-group1/src/main/java/gb/internship/rest/requestcontrollers/user/UserUIGeneralStatisticsController.java
@@ -4,6 +4,8 @@ import gb.internship.rest.dataobjects.PersonGeneralStatistic;
 import gb.internship.rest.db.operations.UsersUiDbOperation;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,21 +34,22 @@ public class UserUIGeneralStatisticsController {
      * @return список всех рейтингов личностей.
      */
     @RequestMapping(value = {"/user/ui/getGeneralStatistics", "/unauthorized/user/ui/getGeneralStatistics"})
-    public List<PersonGeneralStatistic> getGeneralStatistic(@RequestParam(value = "site") String site) {
-        List<PersonGeneralStatistic> resultList = new ArrayList<>();
+    public ResponseEntity<?> getGeneralStatistic(@RequestParam(value = "site") String site) {
+
         if ("".equals(site)) {
             LOG.warn("Error in /user/ui/getGeneralStatistics. sitename is empty.");
-            throw new RuntimeException("Error in /user/ui/getGeneralStatistics. sitename is empty.");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body("Error. site == \"\"");
         }
 
 
         try {
-            resultList = userUiSitesDbOperations.getAllRatesForChoosenSite(site);
+            List<PersonGeneralStatistic> resultList = userUiSitesDbOperations.getAllRatesForChoosenSite(site);;
+            return new ResponseEntity<>(resultList, HttpStatus.OK);
         } catch (Exception ex) {
             LOG.warn("Error at run get general Statistic.");
             ex.printStackTrace();
+            return new ResponseEntity<>("Error at run getGeneralStatistics.", HttpStatus.BAD_REQUEST);
         }
-
-        return resultList;
     }
 }
