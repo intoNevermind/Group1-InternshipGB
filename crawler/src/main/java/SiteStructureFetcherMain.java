25cb6919eef5fbb8e83a8d9ef1e12f91e021fc46
TheFernflower <tanu.foto@gmail.com>
2017-11-19 21:46:25 +0100

    Закомментировала код, дающий ошибки из-за вызова старых версий методов

--- crawler/src/main/java/SiteStructureFetcherMain.java
+++ crawler/src/main/java/SiteStructureFetcherMain.java
@@ -1,10 +1,13 @@
+<<<<<<< HEAD
 import sitefetcher.Downloader;
 import sitefetcher.LinkChecker;
 
+=======
+>>>>>>> Закомментировала код, дающий ошибки из-за вызова старых версий методов
 public class SiteStructureFetcherMain {
     public static void main(String[] args) throws Exception {
         //SiteStructureFetcher.updateSiteStructure("https://www.gazeta.ru");
-        Downloader downloader = new Downloader();
+        /*Downloader downloader = new Downloader();
         System.out.println(downloader.downloadGz("https://lenta.ru/sitemap.xml.gz"));
 
         String url = "https://sportbox.ru";
@@ -13,6 +16,10 @@ public class SiteStructureFetcherMain {
         System.out.println(LinkChecker.isAllowed("https://www.yandex.ru/soft/punto/index.html"));
 
         SiteStructureFetcher siteStructureFetcher = new SiteStructureFetcher();
+<<<<<<< HEAD
         siteStructureFetcher.updateSiteStructure(new Page(0, url), null);
+=======
+        siteStructureFetcher.updateSiteStructure("https://ria.ru/", null);*/
+>>>>>>> Закомментировала код, дающий ошибки из-за вызова старых версий методов
     }
 }
