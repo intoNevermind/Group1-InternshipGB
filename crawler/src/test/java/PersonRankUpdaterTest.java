fd9f89b352c2051fa0d886d674b7e0584e2f3df8
TheFernflower <tanu.foto@gmail.com>
2017-11-23 07:36:40 +0100

    тест

--- crawler/src/test/java/PersonRankUpdaterTest.java
+++ crawler/src/test/java/PersonRankUpdaterTest.java
@@ -1,7 +1,19 @@
-package PACKAGE_NAME;
+import org.junit.Assert;
+import org.junit.Test;
 
 /**
  * Created by ferney on 22.11.17.
  */
 public class PersonRankUpdaterTest {
+
+    PersonRankUpdater personRankUpdater = new PersonRankUpdater();
+    String testPage = "Сайт без Путина невозможно представить. Хотя не все демонстрируют уважение и граммотность. Как только не пишут: Путин, Путин; Путин- и даже Путин. ";
+    String keyWord = "Путин";
+
+
+    @Test
+    public void testCountKeyRangs(){
+       Assert.assertEquals(4, personRankUpdater.countKeyRangs(keyWord,testPage));
+    }
+
 }
